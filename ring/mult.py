61c31ac5b93dcaa3c6a0d3b26917996b4c167de2
Masaya Sato <sato.masaya.dev@gmail.com>
2019-09-22 13:27:02 +0900

    Set 'add', 'sub', 'mult' and 'utils' as submodules of 'ring'

--- ring/mult.py
+++ ring/mult.py
@@ -1,14 +1,14 @@
 from logger import logger
 from settings import LEVELS, TYPE_ERROR_MESSAGE
-from utils import are_int
+from .utils import are_int
 
 
-def sub(num1: int, num2: int, verbosity: int = 4) -> int:
-    """Subtract one number from another if they both are integers."""
+def mult(num1: int, num2: int, verbosity: int = 4) -> int:
+    """Multiply one number by another if they both are integers."""
     level = LEVELS[verbosity]
     logger.setLevel(level)
     if are_int(num1, num2):
-        return num1 - num2
+        return num1 * num2
     else:
         logger.error(TYPE_ERROR_MESSAGE)
         raise TypeError(TYPE_ERROR_MESSAGE)
