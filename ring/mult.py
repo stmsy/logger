70d346091b21852240d09920f9641e6bc3452a01
Masaya Sato <sato.masaya.dev@gmail.com>
2019-09-25 12:40:48 +0900

    Import logger and set dynamically log level properly

--- ring/mult.py
+++ ring/mult.py
@@ -1,13 +1,18 @@
-from logger import logger
-from settings import LEVELS, TYPE_ERROR_MESSAGE
+import logging
+from logging.config import dictConfig
+
+from logger import config, set_level
+from settings import LOGGER_NAME, LEVELS, TYPE_ERROR_MESSAGE
 from .utils import are_int
+
+dictConfig(config)
+logger = logging.getLogger(LOGGER_NAME)
 
 
 def mult(num1: int, num2: int, verbosity: int = 4) -> int:
     """Multiply one number by another if they both are integers."""
-    level = LEVELS[verbosity]
-    logger.setLevel(level)
-    if are_int(num1, num2):
+    set_level(verbosity, logger)
+    if are_int(num1, num2, logger):
         return num1 * num2
     else:
         logger.error(TYPE_ERROR_MESSAGE)
