9333f75c4c634591a9b4eac71b2bda258e8d578c
Masaya Sato <sato.masaya.dev@gmail.com>
2019-09-26 10:48:09 +0900

    Import logger name

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 
+from params import LOGGER_NAME
+
 PWD = Path.cwd()
 LOG_DIR = PWD.joinpath('logs')
 CONFIG_DIR = PWD.joinpath('config')
@@ -9,7 +11,5 @@ LEVELS = {
     0: logging.NOTSET, 1: logging.DEBUG, 2: logging.INFO,
     3: logging.WARNING, 4: logging.ERROR, 5: logging.CRITICAL
 }
-
-LOGGER_NAME = 'sampleLogger'
 
 TYPE_ERROR_MESSAGE = "both args must be integers"
