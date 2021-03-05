f7db43febf43778e06b4cda3563e85b68f017cac
Masaya Sato <sato.masaya.dev@gmail.com>
2021-03-05 18:26:53 +0900

    Enable to log with specified verbosity

--- sample.py
+++ sample.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
 
-from add import add
-from mult import mult
-from logger import logger
-from sub import sub
+from ring.add import add
+from ring.mult import mult
+from ring.sub import sub
 
 
 if __name__ == '__main__':
-    # Log messages with several levels
-    logger.log(20, "info")
-    logger.log(30, "warning")
-    logger.log(50, "critical")
-    logger.info("info")
-    logger.warning("warning")
-    logger.critical("critical")
-
     # Log messages from another module
-    add(1, 2)
-    sub(1, 2)
-    mult(1.0, 2)
+    add(1.0, 2, verbosity=5)
